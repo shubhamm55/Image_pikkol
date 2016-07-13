c42a987d4bf2298a2d57f4e4678d218eb9eacf96
Shubham <shubh12197@gmail.com>
2016-07-13 16:47:11 +0530

    Upload to server  after click

--- app/src/main/java/pikkol/img4/MainActivity.java
+++ app/src/main/java/pikkol/img4/MainActivity.java
@@ -1,46 +1,83 @@
 package pikkol.img4;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
 import android.widget.ImageView;
+import android.widget.Toast;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import cz.msebera.android.httpclient.Header;
+
 public class MainActivity extends AppCompatActivity {
     ImageView imageView;
-    Button photo;
-    static final int REQUEST_CAMERA=1;
+    Button photo, load, upload;
+    static final int REQUEST_CAMERA = 1;
     String mCurrentPhotoPath;
     Uri photoURI;
     File photoFile = null;
+
+    //trying to upoad on server through gallery
+    ProgressDialog prgDialog;
+    String encodedString;
+    RequestParams params = new RequestParams();
+    String  fileName;
+    Bitmap bitmap;
+    private static int RESULT_LOAD_IMG = 1;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        prgDialog = new ProgressDialog(this);
+        // Set Cancelable as False
+        prgDialog.setCancelable(false);
 
+        /*load = (Button) this.findViewById(R.id.load);
+        load.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadImagefromGallery();
+            }
+        });*/
+        upload = (Button) this.findViewById(R.id.upload);
+        upload.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                uploadImage();
+            }
+        });
         imageView = (ImageView) this.findViewById(R.id.image);
         photo = (Button) this.findViewById(R.id.button);
-        photo.setOnClickListener(new View.OnClickListener() {
+       photo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dispatchTakePictureIntent();
@@ -50,16 +87,57 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_CAMERA && resultCode== RESULT_OK) {
 
-       //     setPic();// temporarily removed
-                        //result_ok=-1 working fine here,actual problem is data=null thus no view...
+        /*try {
+            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK
+                    && null != data) {
+                // Get the Image from data
+
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+                // Get the cursor
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                // Move to first row
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                imgPath = cursor.getString(columnIndex);
+                cursor.close();
+                // Set the Image in ImageView
+                imageView.setImageBitmap(BitmapFactory
+                        .decodeFile(imgPath));
+                // Get the Image's file name
+                String fileNameSegments[] = imgPath.split("/");
+                fileName = fileNameSegments[fileNameSegments.length - 1];
+                // Put file name in Async Http Post Param which will used in Php web app
+                params.put("filename", fileName);
+
+            } else {
+                Toast.makeText(this, "You haven't picked Image",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+            catch(Exception ex){
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
+                        .show();
+            }
+
+
+
+    }*/
+        if (requestCode == REQUEST_CAMERA && resultCode == RESULT_OK) {
+
+            setPic();
+            //result_ok=-1 working fine here,actual problem is data=null thus no view...
 
             galleryAddPic(photoFile.toString());
             Log.w("OOPS", "BAZINGAA");
 
         }
-        }
+    }
+
 
     public void galleryAddPic(String file) {
         File f = new File(file);
@@ -67,22 +145,7 @@ public class MainActivity extends AppCompatActivity {
         Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,contentUri);
         sendBroadcast(mediaScanIntent);
     }
-   /* protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
 
-        // save file url in bundle as it will be null on screen orientation
-        // changes
-        outState.putParcelable("file_uri", photoURI);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-
-        // get the file url
-        photoURI = savedInstanceState.getParcelable("file_uri");
-    }
-    */
 
 
     public void dispatchTakePictureIntent() {
@@ -117,6 +180,125 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+/*    public void loadImagefromGallery() {
+        // Create intent to Open Image applications like Gallery, Google Photos
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // Start the Intent
+        startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+    }
+
+  */  public void uploadImage() {
+        // When Image is selected from Gallery
+        if (mCurrentPhotoPath != null && !mCurrentPhotoPath.isEmpty()) {
+            prgDialog.setMessage("Converting Image to Binary Data");
+            prgDialog.show();
+            // Convert image to String using Base64
+            encodeImagetoString();
+            // When Image is not selected from Gallery
+        } else {
+            Toast.makeText(
+                    getApplicationContext(),
+                    "You must select image from gallery before you try to upload",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void encodeImagetoString() {
+        new AsyncTask<Void, Void, String>() {
+
+            protected void onPreExecute() {
+
+            }
+
+            @Override
+            protected String doInBackground(Void... params) {
+                BitmapFactory.Options options ;
+                options = new BitmapFactory.Options();
+                options.inSampleSize = 3;
+                bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath,
+                        options);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                // Must compress the Image to reduce image size to make upload easy
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] byte_arr = stream.toByteArray();
+                // Encode Image to String
+                encodedString = Base64.encodeToString(byte_arr, 0);
+                return "";
+            }
+
+            @Override
+            protected void onPostExecute(String msg) {
+                prgDialog.setMessage("Calling Upload");
+                // Put converted Image string into Async Http Post param
+                params.put("image", encodedString);
+                // Trigger Image upload
+                triggerImageUpload();
+            }
+        }.execute(null, null, null);
+    }
+
+    public void triggerImageUpload() {
+        makeHTTPCall();
+    }
+
+
+    // Make Http call to upload Image to Php server
+    public void makeHTTPCall() {
+        prgDialog.setMessage("Invoking Php");
+        AsyncHttpClient client = new AsyncHttpClient();
+        // Don't forget to change the IP address to your LAN address. Port no as well.
+        client.post("http://168.192.56.102:8600/imgupload/upload_image.php",
+                params, new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                        prgDialog.hide();
+                        Toast.makeText(getApplicationContext(), statusCode,
+                                Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                        // Hide Progress Dialog
+                        prgDialog.hide();
+                        // When Http response code is '404'
+                        if (statusCode == 404) {
+                            Toast.makeText(getApplicationContext(),
+                                    "Requested resource not found",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                        // When Http response code is '500'
+                        else if (statusCode == 500) {
+                            Toast.makeText(getApplicationContext(),
+                                    "Something went wrong at server end",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                        // When Http response code other than 404, 500
+                        else {
+                            Toast.makeText(
+                                    getApplicationContext(),
+                                    "Error Occured n Most Common Error: n1. Device not connected to Internetn2. Web App is not deployed in App servern3. App server is not runningn HTTP Status code : "
+                                            + statusCode, Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    }
+
+                });
+    }
+
+
+
+
+                    // When the response returned by REST has Http
+                    // response code '200'
+
+                    // When the response returned by REST has Http
+                    // response code other than '200' such as '404',
+                    // '500' or '403' etc
+
+
+
 
 
 
@@ -132,10 +314,11 @@ public class MainActivity extends AppCompatActivity {
         );
 
         // Save a file: path for use with ACTION_VIEW intents
+
         mCurrentPhotoPath =  image.getAbsolutePath();
         return image;
     }
-    /*private void setPic() {//temporarily removed to check with json
+    private void setPic() {
         // Get the dimensions of the View
         int targetW = imageView.getWidth();
         int targetH = imageView.getHeight();
@@ -153,10 +336,10 @@ public class MainActivity extends AppCompatActivity {
         // Decode the image file into a Bitmap sized to fill the View
         bmOptions.inJustDecodeBounds = false;
         bmOptions.inSampleSize = scaleFactor;
-        bmOptions.inPurgeable = true;
+       // bmOptions.inPurgeable = true;
 
         Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
         imageView.setImageBitmap(bitmap);
-    }*/
+    }
 
 }
